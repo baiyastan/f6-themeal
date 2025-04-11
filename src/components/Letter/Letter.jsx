ea8f49920d9f48eb07695a9dc44c908cf6f9c6c8
Bayastan <bsbaha195@mail.com>
2025-04-11 09:30:22 +0600

    add new user

--- src/components/Letter/Letter.jsx
+++ src/components/Letter/Letter.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { letter } from '../../data/user'
+import { Link } from 'react-router-dom'
 
 function Letter() {
   return (
     <div>
-      
+      {
+        letter.map((item, index) => (
+            <p key={index}>
+                <Link>{item}</Link>
+            </p>
+        ))
+      }
     </div>
   )
 }
